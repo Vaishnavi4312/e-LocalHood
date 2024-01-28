b1d7503c0b3821a29aff859c1173613fe849151c
Vaishnavi4312 <gurav.vaishnavi43@gmail.com>
2024-01-28 19:20:30 +0530

    basic project structure

---
+++ src/Message.tsx
@@ -0,0 +1,10 @@
+// PascalCasing
+function message() {
+    // JSX : JavaScript XML
+    const name = 'Vaishnavi';
+    if (name)
+    return <h1>Hello {name}</h1>;
+    return <h1>Hello World</h1>
+}
+
+export default message;
