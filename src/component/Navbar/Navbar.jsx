337a7b551808f7b9beb5c67d13caae68f46726bc
Vaishnavi4312 <gurav.vaishnavi43@gmail.com>
2024-02-08 14:06:55 +0530

    Project information

--- src/component/Navbar/Navbar.jsx
+++ src/component/Navbar/Navbar.jsx
@@ -1,26 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Navbar.css'
 import logo from '../assets/logo.png'
-import card_icon from '../assets/card_icon.png'
+import cart_icon from '../assets/cart_icon.png' 
   const Navbar = () => {
+    const[menu, setMenu] = useState("")
     return (
+      <div>
     <div className='navbar'>
-      <div className="nav-logo">
-        <img src ={logo} width={100} height={70} alt = "" />
-       </div>
+      <div className='nav-logo'>
+        <img src ={logo} width={70} height={70} alt = "" />
+        </div>
       <ul className="nav-menu">
-        <li>shop</li>
-        <li>Men</li>
-        <li>Women</li>
-        <li>Kids</li>
-        <li>Electronic</li>
-        <li>Bakery Items</li>
+        <li>Electronic Appliance Repair</li>
         <li>Grocery</li>
-      </ul>
-      <div className='nav-login-cart'>
+        <li>Doctor</li>
+        <li>Plumber</li>
+        <li>Parlour</li>      
+        </ul>
+        <span>           
+        <img className='overlay' src={cart_icon} width={60} height={60} alt = "" /> 0
+        </span>
         <button>Login</button>
+        <button>Sign Up</button>
+       {/* <span> */}
+        
+        {/* </span> */}
+        </div>
+      <div>
       </div>
-    </div>
+      </div>
+    
   );
 }
 export default Navbar;
